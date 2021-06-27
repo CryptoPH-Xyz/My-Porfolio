2de5b65de25ceb06b6edceb2e765470e645b6cda
Xyz Fiegalan <xyzfiegalan@gmail.com>
2021-06-27 13:39:19 +0800

    Education and Work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Experience from './components/Experience';
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Experience />
     </>
   );
 }
---
+++ src/components/Experience.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react'
+
+export default function Experience() {
+    
+    const [education, setEducation] = useState(true);
+    const [work, setWork] = useState(false);
+
+    const workSwitch = () => {
+        setEducation(false);
+        setWork(true);
+    }
+
+    const educationSwitch = () => {
+        setEducation(true);
+        setWork(false);
+    }
+
+
+    
+    return (
+        <div className="experience section" id="experience">
+            <h2 className="section-title">Experience</h2>
+            <span className="section-subtitle">My Journey</span>
+
+            <div className="experience-container container">
+                <div className="experience-tabs">
+                    <div className="experience-button button-flex" onClick={educationSwitch}>
+                        <i className="uil uil-graduation-cap experience-icon"></i> Education
+                    </div>
+                    <div className="experience-button button-flex" onClick={workSwitch}>
+                        <i className="uil uil-suitcase-alt experience-icon"></i> Work
+                    </div>
+                </div>
+                {education ? 
+                    <div className="experience-content">
+                        <div className="experience-data">
+                            <div className="data-right">
+                                <div className="experience-calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2014 - 2018
+                                </div>
+                                <h3 className="experience-title">(BS) Business Administration Major in Economics</h3>
+                                <span className="experience-subtitle">Philippines - Central Luzon State University <br/> Cum Laude</span>
+                            </div>
+                            <div>
+                                <span className="experience-rounder"></span>
+                                <span className="experience-line"></span>
+                            </div>
+                        </div>
+
+                        <div className="experience-data">
+                            <div></div>
+                            <div>
+                                <span className="experience-rounder"></span>
+                                <span className="experience-line"></span>
+                            </div>
+                            <div>
+                                <div className="experience-calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2019 - 2021
+                                </div>
+                                <h3 className="experience-title">(MA) Economics</h3>
+                                <span className="experience-subtitle">Philippines - University of the Philippines, Diliman<br/> PCED Fellow</span>
+                            </div>
+                        </div>
+
+                        <div className="experience-data">
+                            <div className="data-right">
+                                <div className="experience-calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2021 - Present
+                                </div>
+                                <h3 className="experience-title">Blockchain Student</h3>
+                                <span className="experience-subtitle">Sweden - Ivan on Tech Blockchain Academy (online)</span>
+                            </div>
+                            <div>
+                                <span className="experience-rounder"></span>
+                            </div>
+                        </div>
+                    </div>
+                : null}
+
+                {work ? 
+                    <div className="experience-content"> 
+                        <div className="experience-data">
+                        <div></div>
+                            <div>
+                                <span className="experience-rounder"></span>
+                                <span className="experience-line"></span>      
+                            </div>
+                            <div>
+                                <div className="experience-calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2018 (Jan - March)
+                                </div>
+                                <h3 className="experience-title">Billing Specialist (Trainee)</h3>
+                                <span className="experience-subtitle">Philippines - AFPMBAI</span>
+                            </div>
+                        </div>
+
+                        <div className="experience-data">
+                            <div className="data-right">
+                                <div className="experience-calendar">
+                                    <i className="uil uil-calendar-alt"></i> 2021 (Jan - June)
+                                </div>
+                                <h3 className="experience-title">Graduate Teaching Fellow</h3>
+                                <span className="experience-subtitle">Philippines - University of the Philippines, Diliman</span>
+                            </div>
+                            <div>
+                                <span className="experience-rounder"></span>
+                            </div>
+                        </div>
+                    </div>
+                : null}
+            </div>
+        </div>
+    )
+}
